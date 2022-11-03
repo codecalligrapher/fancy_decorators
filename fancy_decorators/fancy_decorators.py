a96205e5b55a6764788c14166faabca05d1bafe5
Aadidev Sooknanan <a.aadi380@gmail.com>
2022-11-03 19:43:35 -0400

    fix module contents

--- fancy_decorators/fancy_decorators.py
+++ fancy_decorators/fancy_decorators.py
@@ -1,34 +1,10 @@
-"""This decorator takes an arbitrary config with parameters,
-e.g.
-  conf = {
-    'over': [0.1, 0.5], 
-    'under': [0.2, 0.5]
-  }
- 
- There is no restriction on the number of keys the above has, it will calculate a cross
- product of all the options
-
- AND a metrics_df created as follows:
- conf = {
-    # your grid-search params
-  }
-  
-  # this allows ease of indexing
-  values = conf.values()
-  keys = conf.keys()
-  midx =  MultiIndex.from_product(list(values), names=list(keys))
-  metrics_df = DataFrame(index=midx)
-  
-  #HOW TO USE:
-  Decorate your funciton
-  @log_metrics(conf, metrics_df)
-  def evaluate_sampling(X_train, y_train, X_test, y_test, over, under):
-    ...
-    return dict() <- this MUST be a dictionary of metrics:metric_value
- 
-"""
-
 def log_metrics(conf,metrics_df):
+    '''Top-level decorator for grid-search evaluation
+    
+    :param conf: dictionary in function_parameter_name:list_of_values format
+    :param metrics_df: constructed as shown in the examples
+    
+    '''
     import itertools
     from tqdm import tqdm
 
